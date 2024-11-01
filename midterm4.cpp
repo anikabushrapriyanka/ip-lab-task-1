07fa22ee16671abb0521acd1312ce74140c920a6
anikabushrapriyanka <anikabushrapriyanka@gmail.com>
2024-11-01 23:40:37 +0600

    Add files via upload

---
+++ midterm4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1=80, num2=10;
+    if(num1%num2==0)
+    {
+        cout<<"first is the multiple of the second: ";
+    } else {
+        cout<<"first is not the multiple of the second: ";
+    }
+}
